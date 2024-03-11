cd1d17c92e7f4a5c31a5ce8ee4acb300ae3dd81a
MuragayyaSG <119426895+MuragayyaSG@users.noreply.github.com>
2024-03-11 20:21:50 +0530

    Update and rename 1d chokka (merge,sort,remove).c to Remove repeated in 1D array.c

--- Remove repeated in 1D array.c
+++ Remove repeated in 1D array.c
@@ -1,26 +1,17 @@
+// C code to remove an repeated elements in an array
+
 #include<stdio.h>
 #include<stdlib.h>
 void read(int a[100],int n);
 void disp(int a[100],int n);
-int merge(int a[100],int c[100],int m,int k);
-void sort(int c[100],int n);
 int removerepeated(int c[100],int m);
 main()
 {
-int a[100],b[100],c[100],m,n,t,k=0,s;
-printf("enter the size of set 1\n");
-scanf("%d",&m);
-read(a,m);
-printf("enter the size of set 2\n");
+int a[100],n,s;
+printf("enter the size of array\n");
 scanf("%d",&n);
-read(b,n);
-t=merge(a,c,m,k);
-t=merge(b,c,n,t);
-printf("After merging\n");
-disp(c,t);
-sort(c,t);
-printf("After sorting\n");
-disp(c,t);
+read(a,n);
+ disp(a,n);
 s=removerepeated(c,t);
  printf("\nafter removing duplicate elements");
 
@@ -39,31 +30,7 @@ void disp(int a[100],int n)
     for(i=0;i<n;i++)
         printf("%d  ",a[i]);
 }
-int merge(int a[100],int c[100],int m,int n)
-{int i,k;
-    for(i=0;i<m;i++)
-    {
-        c[k]=a[i];
-        k++;
-    }
-    return k;
-}
 
-void sort(int c[100],int n)
-{
-    int i,j,t1;
-    for(i=0;i<=n-2;i++)
-    {
-    for(j=0;j<=n-2-i;j++)
-    {
-        if(c[j]>c[j+1])
-           {
-
-            t1=c[j];
-        c[j]=c[j+1];
-        c[j+1]=t1;
-        }}}
-}
 int removerepeated(int c[100],int m)
 {
       int i,j,k;
